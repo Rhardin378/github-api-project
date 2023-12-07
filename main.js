3f75b11d8b90847fc5d8dec741252b9c1673f98a
Ryan Hardin <rhardin378@gmail.com>
2023-12-07 06:44:22 -0600

    working app....should've made commits after each working function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const engineersDiv = document.querySelector(".reactEngineers");
 const searchBtn = document.querySelector(".search-btn");
 const searchInput = document.querySelector(".searchInput");
 
+let reactCreators = [];
+
 // fetch request to : https://api.github.com/repos/facebook/react/commits/${query}
 // create an object to display data from it
 // create template to use on each search result
@@ -17,6 +19,54 @@ const searchInput = document.querySelector(".searchInput");
 // insertAdjacentHTML
 
 searchBtn.addEventListener("click", function (e) {
-  console.log(searchInput.value);
+  const search = searchInput.value;
+  fetchCommit(search);
+
   searchInput.value = "";
 });
+
+const fetchCommit = function (query) {
+  const url = ` https://api.github.com/repos/facebook/react/commits/${query}`;
+  fetch(url, {
+    method: "GET",
+    dataType: "json",
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      addCreator(data);
+    });
+};
+
+const addCreator = function (data) {
+  const reactCreator = {
+    sha: data.sha ? data.sha : null,
+    author: data.author.login ? data.author.login : null,
+    avatar: data.author.avatar_url ? data.author.avatar_url : null,
+  };
+  reactCreators.push(reactCreator);
+  renderCreators();
+};
+
+let renderCreators = function () {
+  engineersDiv.replaceChildren();
+
+  reactCreators.forEach(function (creator) {
+    const template = `
+    <div class= "col-4">
+    <div class="card border border-4 border-dark mx-2">
+      <div class="card-header">${creator.author}</div>
+      <div class="card-body">
+        <img
+          src= ${creator.avatar}
+          class="card-img"
+          alt="..."
+        />
+        </div>
+        </div>
+      </div>`;
+
+    engineersDiv.insertAdjacentHTML("beforeend", template);
+
+    console.log(creator);
+  });
+};
